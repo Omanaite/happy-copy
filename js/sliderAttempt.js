7a31ed54eed674098964aa3805e69c199fbff63c
Doug <dmhwebb@gmail.com>
2023-03-04 18:35:54 +0100

    make work for any number of cards

--- js/sliderAttempt.js
+++ js/sliderAttempt.js
@@ -3,20 +3,19 @@ const rightButton = document.querySelector("#sliderRight");
 
 function slideRight() {
     let gameCards = document.body.querySelectorAll(".gameCard");
-    gameCards[4].classList.add("gameCard__shrunk");
-    setTimeout(() => {document.querySelector(".footer-slider-cardContainer").prepend(gameCards[4])}, 500);
-    setTimeout(() => {gameCards[4].classList.remove("gameCard__shrunk")}, 550);
+    let cardNo = (gameCards.length - 1);
+    gameCards[cardNo].classList.add("gameCard__shrunk");
+    setTimeout(() => {document.querySelector(".footer-slider-cardContainer").prepend(gameCards[cardNo])}, 500);
+    setTimeout(() => {gameCards[cardNo].classList.remove("gameCard__shrunk")}, 550);
 };
 
 function slideLeft() {
     let gameCards = document.body.querySelectorAll(".gameCard");
-    gameCards[0].classList.add("gameCard__shrunk");
-    setTimeout(() => {document.querySelector(".footer-slider-cardContainer").append(gameCards[0])}, 500);
-    setTimeout(() => {gameCards[0].classList.remove("gameCard__shrunk")}, 550);
+    let cardNo = 0;
+    gameCards[cardNo].classList.add("gameCard__shrunk");
+    setTimeout(() => {document.querySelector(".footer-slider-cardContainer").append(gameCards[cardNo])}, 500);
+    setTimeout(() => {gameCards[cardNo].classList.remove("gameCard__shrunk")}, 550);
 };
-
 
 leftButton.addEventListener("click", slideLeft);
 rightButton.addEventListener("click", slideRight);
-
-
