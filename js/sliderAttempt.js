c89ff4c5b37d22d49834f4408f370e4f80e0d124
Doug <dmhwebb@gmail.com>
2023-03-04 18:32:55 +0100

    working prototype

--- js/sliderAttempt.js
+++ js/sliderAttempt.js
@@ -1,3 +1,22 @@
+const leftButton = document.querySelector("#sliderLeft");
+const rightButton = document.querySelector("#sliderRight");
 
-const gameCards = document.body.querySelectorAll(".gameCard");
-console.log(gameCards);
+function slideRight() {
+    let gameCards = document.body.querySelectorAll(".gameCard");
+    gameCards[4].classList.add("gameCard__shrunk");
+    setTimeout(() => {document.querySelector(".footer-slider-cardContainer").prepend(gameCards[4])}, 500);
+    setTimeout(() => {gameCards[4].classList.remove("gameCard__shrunk")}, 550);
+};
+
+function slideLeft() {
+    let gameCards = document.body.querySelectorAll(".gameCard");
+    gameCards[0].classList.add("gameCard__shrunk");
+    setTimeout(() => {document.querySelector(".footer-slider-cardContainer").append(gameCards[0])}, 500);
+    setTimeout(() => {gameCards[0].classList.remove("gameCard__shrunk")}, 550);
+};
+
+
+leftButton.addEventListener("click", slideLeft);
+rightButton.addEventListener("click", slideRight);
+
+
