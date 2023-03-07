ec8771b1ee5a51315a2d8db531251d373ce47091
Doug <dmhwebb@gmail.com>
2023-03-07 14:50:05 +0100

    starting to tailwind

--- main.js
+++ main.js
@@ -1,16 +1,2 @@
-$(document).ready(function() {
+import './style.css'
 
-    // select all question divs
-    $(".question").click(function() {
-
-        // select next content div
-        var content = $(this).next(".content");
-
-        // toggle display
-        content.toggle();
-
-        // optional: change icon or text of question div
-
-    });
-
-});
