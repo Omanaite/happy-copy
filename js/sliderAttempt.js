7ca7dbfef7b4120b943641df68de0845b34813c1
Doug <dmhwebb@gmail.com>
2023-03-08 11:22:18 +0100

    refactor slide with advice from Franz

--- js/sliderAttempt.js
+++ js/sliderAttempt.js
@@ -1,21 +1,78 @@
 const leftButton = document.querySelector("#sliderLeft");
 const rightButton = document.querySelector("#sliderRight");
 
+let currIndex = 0;
+let prevIndex = 0;
+
+const gameCards = document.body.querySelectorAll(".gameCard");
+
+function modulus(n, m) {
+    return ((n % m) + m) % m;
+}
+
 function slideRight() {
-    let gameCards = document.body.querySelectorAll(".gameCard");
-    let cardNo = (gameCards.length - 1);
-    gameCards[cardNo].classList.add("gameCard__shrunk");
-    setTimeout(() => {document.querySelector(".footer-slider-cardContainer").prepend(gameCards[cardNo])}, 500);
-    setTimeout(() => {gameCards[cardNo].classList.remove("gameCard__shrunk")}, 550);
+    prevIndex = currIndex;
+    currIndex = modulus(currIndex + 1, gameCards.length);
+    renderCarousel(currIndex);
 };
 
 function slideLeft() {
-    let gameCards = document.body.querySelectorAll(".gameCard");
-    let cardNo = 0;
-    gameCards[cardNo].classList.add("gameCard__shrunk");
-    setTimeout(() => {document.querySelector(".footer-slider-cardContainer").append(gameCards[cardNo])}, 500);
-    setTimeout(() => {gameCards[cardNo].classList.remove("gameCard__shrunk")}, 550);
+    prevIndex = currIndex;
+    currIndex = modulus(currIndex - 1, gameCards.length)
+    renderCarousel(currIndex);
 };
+
+const cards = [
+    {
+        cardImage: "assets/images/yugioh.svg",
+        titleImage: "assets/images/yugioh.webp",
+    },
+    {
+        cardImage: "assets/images/magic.svg",
+        titleImage: "assets/images/magic.webp",
+    },
+    {
+        cardImage: "assets/images/pokemon.svg",
+        titleImage: "assets/images/pokemon.webp",
+    },
+
+]
+
+const cardContainer = document.querySelector(".footer-slider-cardContainer");
+
+function removeChildren(parent) {
+    while (parent.lastChild) {
+        parent.removeChild(parent.lastChild);
+    }
+}
+
+function createCard(titleImage, cardImage) {
+    const cardEl = document.createElement("div")
+    const cardImageEl = document.createElement("img")
+    cardImageEl.src = cardImage;
+    const titleImageEl = document.createElement("img")
+    titleImageEl.src = titleImage;
+    cardImageEl.style.width = "7rem"
+    titleImageEl.style.width = "9rem"
+    cardEl.classList.add("gameCard");
+    cardEl.appendChild(cardImageEl);
+    cardEl.appendChild(titleImageEl);
+    return cardEl;
+}
+
+function renderCarousel(currIndex) {
+    removeChildren(cardContainer);
+    const displayCards = cards.slice(currIndex, modulus(currIndex + 3, cards.length + 1));
+    // console.log(currIndex, modulus(currIndex + 3, cards.length));
+    for (const card of displayCards) {
+        cardContainer.appendChild(createCard(card.titleImage, card.cardImage));
+    }
+}
 
 leftButton.addEventListener("click", slideLeft);
 rightButton.addEventListener("click", slideRight);
+
+renderCarousel(currIndex);
+
+
+// cardContainer.innerHTML = "asfadssa";
